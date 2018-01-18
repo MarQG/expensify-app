b6cad62b274fb884c5b405da4e9927ea5f1a28c6
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-18 08:44:54 -0600

    Setup production webpack build

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -65,8 +65,8 @@ const mapDispatchToProps = (dispatch) => ({
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
-    setStartDate: () => dispatch(setStartDate(startDate)),
-    setEndDate: () => dispatch(setEndDate(endDate))
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
