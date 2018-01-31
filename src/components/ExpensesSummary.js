4851faaf77ef834436620eb8a45d3660e19387a9
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-31 12:00:34 -0600

    Added show hidden expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,13 +6,15 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
-    const expensesWord = expenseCount === 1 ? 'expense' : 'expenses'; 
+export const ExpensesSummary = ({ expenseCount, expensesTotal, expensesCountTotal }) => {
+    const expensesWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const expensesCountWord = expensesCountTotal === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
         <div className="page-header">
             <div className="content-container">
-                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expensesWord} totalling <span>{formattedExpensesTotal}</span> </h1>
+                <h1 className="page-header__title">Showing <span>{expenseCount}</span> of {expensesWord} totalling <span>{formattedExpensesTotal}</span> </h1>
+                <h1 className="page-header__title"><span>{expensesCountTotal}</span> hidden {expensesCountWord}  not being shown.</h1>
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -23,9 +25,11 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    const totalExpensesCount = state.expenses.length - visibleExpenses.length;
     return{
         expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses),
+        expensesCountTotal: totalExpensesCount
     }
 };
 
